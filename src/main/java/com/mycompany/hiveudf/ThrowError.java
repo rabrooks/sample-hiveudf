5504e7592df1c253e934df9bc9c99d3331f836f8
Carter Shanklin <carter@HW11972.local>
2017-01-11 08:18:04 -0800

    Change to deterministic so it creates runtime failures.

--- src/main/java/com/mycompany/hiveudf/ThrowError.java
+++ src/main/java/com/mycompany/hiveudf/ThrowError.java
@@ -1,10 +1,17 @@
 package com.mycompany.hiveudf;
 
+import java.util.Random;
+ 
 import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.io.Text;
- 
+
 public final class ThrowError extends UDF {
+  private static Random rand = new Random();
+
   public Text evaluate(final Text s) throws Exception {
-    throw new Error("FAKE_ERROR: " + s.toString());
+    if (rand.nextInt(100) < 10) {
+      throw new Error("FAKE_ERROR: " + s.toString());
+    }
+    return new Text("SKIP");
   }
 }
